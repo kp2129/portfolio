45fe66e5e775023c7877ca11049e7025f3f4841e
kp2129 <kristapspolis0@gmail.com>
2024-08-26 21:45:15 +0300

    'main_page'

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { IconMenu2 } from '@tabler/icons-react';
+import { useState } from 'react';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App dot">
+      <header className="header">
+        <nav className="navigation">
+          <div>
+            <p className="logo work-sans-bold">kp2129</p>
+          </div>
+          <div className="menu-icon" onClick={toggleMenu}>
+            <IconMenu2 stroke={2} />
+          </div>
+          <div className={`links ${menuOpen ? 'open' : ''}`}>
+            <a href="#AboutMe">About me</a>
+            <a href="#Projects">Projects</a>
+            <a href="#Education">Education</a>
+            <a href="#Contacts">Contacts</a>
+          </div>
+        </nav>
       </header>
+      <main className="main-content">
+        {/* Your main content here */}
+      </main>
     </div>
   );
 }
